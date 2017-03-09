30608b7c530c1e2b11da0a9a61a00e5da2fe60f8
Robin Kamboj <dev.robinkamboj@gmail.com>
2017-03-10 00:52:45 +0530

    ...

---
+++ app/src/main/java/com/appbusters/robinkamboj/popularmoviesapp/model/Movie.java
@@ -0,0 +1,392 @@
+package com.appbusters.robinkamboj.popularmoviesapp.model;
+
+import java.util.List;
+
+public class Movie {
+
+    public Movie(boolean adult, String backdrop_path, Object belongs_to_collection, int budget, String homepage, int id, String imdb_id, String original_language, String original_title, String overview, double popularity, String poster_path, String release_date, int revenue, int runtime, String status, String tagline, String title, boolean video, double vote_average, int vote_count, List<GenresBean> genres, List<ProductionCompaniesBean> production_companies, List<ProductionCountriesBean> production_countries, List<SpokenLanguagesBean> spoken_languages) {
+        this.adult = adult;
+        this.backdrop_path = backdrop_path;
+        this.belongs_to_collection = belongs_to_collection;
+        this.budget = budget;
+        this.homepage = homepage;
+        this.id = id;
+        this.imdb_id = imdb_id;
+        this.original_language = original_language;
+        this.original_title = original_title;
+        this.overview = overview;
+        this.popularity = popularity;
+        this.poster_path = poster_path;
+        this.release_date = release_date;
+        this.revenue = revenue;
+        this.runtime = runtime;
+        this.status = status;
+        this.tagline = tagline;
+        this.title = title;
+        this.video = video;
+        this.vote_average = vote_average;
+        this.vote_count = vote_count;
+        this.genres = genres;
+        this.production_companies = production_companies;
+        this.production_countries = production_countries;
+        this.spoken_languages = spoken_languages;
+    }
+
+    /**
+     * adult : false
+     * backdrop_path : /wSJPjqp2AZWQ6REaqkMuXsCIs64.jpg
+     * belongs_to_collection : null
+     * budget : 63000000
+     * genres : [{"id":18,"name":"Drama"}]
+     * homepage : http://www.foxmovies.com/movies/fight-club
+     * id : 550
+     * imdb_id : tt0137523
+     * original_language : en
+     * original_title : Fight Club
+     * overview : A ticking-time-bomb insomniac and a slippery soap salesman channel primal male aggression into a shocking new form of therapy. Their concept catches on, with underground "fight clubs" forming in every town, until an eccentric gets in the way and ignites an out-of-control spiral toward oblivion.
+     * popularity : 7.64686
+     * poster_path : /adw6Lq9FiC9zjYEpOqfq03ituwp.jpg
+     * production_companies : [{"name":"Regency Enterprises","id":508},{"name":"Fox 2000 Pictures","id":711},{"name":"Taurus Film","id":20555},{"name":"Linson Films","id":54050},{"name":"Atman Entertainment","id":54051},{"name":"Knickerbocker Films","id":54052}]
+     * production_countries : [{"iso_3166_1":"DE","name":"Germany"},{"iso_3166_1":"US","name":"United States of America"}]
+     * release_date : 1999-10-15
+     * revenue : 100853753
+     * runtime : 139
+     * spoken_languages : [{"iso_639_1":"en","name":"English"}]
+     * status : Released
+     * tagline : How much can you know about yourself if you've never been in a fight?
+     * title : Fight Club
+     * video : false
+     * vote_average : 8.1
+     * vote_count : 6857
+     */
+
+    private boolean adult;
+    private String backdrop_path;
+    private Object belongs_to_collection;
+    private int budget;
+    private String homepage;
+    private int id;
+    private String imdb_id;
+    private String original_language;
+    private String original_title;
+    private String overview;
+    private double popularity;
+    private String poster_path;
+    private String release_date;
+    private int revenue;
+    private int runtime;
+    private String status;
+    private String tagline;
+    private String title;
+    private boolean video;
+    private double vote_average;
+    private int vote_count;
+    private List<GenresBean> genres;
+    private List<ProductionCompaniesBean> production_companies;
+    private List<ProductionCountriesBean> production_countries;
+    private List<SpokenLanguagesBean> spoken_languages;
+
+    public boolean isAdult() {
+        return adult;
+    }
+
+    public void setAdult(boolean adult) {
+        this.adult = adult;
+    }
+
+    public String getBackdrop_path() {
+        return backdrop_path;
+    }
+
+    public void setBackdrop_path(String backdrop_path) {
+        this.backdrop_path = backdrop_path;
+    }
+
+    public Object getBelongs_to_collection() {
+        return belongs_to_collection;
+    }
+
+    public void setBelongs_to_collection(Object belongs_to_collection) {
+        this.belongs_to_collection = belongs_to_collection;
+    }
+
+    public int getBudget() {
+        return budget;
+    }
+
+    public void setBudget(int budget) {
+        this.budget = budget;
+    }
+
+    public String getHomepage() {
+        return homepage;
+    }
+
+    public void setHomepage(String homepage) {
+        this.homepage = homepage;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getImdb_id() {
+        return imdb_id;
+    }
+
+    public void setImdb_id(String imdb_id) {
+        this.imdb_id = imdb_id;
+    }
+
+    public String getOriginal_language() {
+        return original_language;
+    }
+
+    public void setOriginal_language(String original_language) {
+        this.original_language = original_language;
+    }
+
+    public String getOriginal_title() {
+        return original_title;
+    }
+
+    public void setOriginal_title(String original_title) {
+        this.original_title = original_title;
+    }
+
+    public String getOverview() {
+        return overview;
+    }
+
+    public void setOverview(String overview) {
+        this.overview = overview;
+    }
+
+    public double getPopularity() {
+        return popularity;
+    }
+
+    public void setPopularity(double popularity) {
+        this.popularity = popularity;
+    }
+
+    public String getPoster_path() {
+        return poster_path;
+    }
+
+    public void setPoster_path(String poster_path) {
+        this.poster_path = poster_path;
+    }
+
+    public String getRelease_date() {
+        return release_date;
+    }
+
+    public void setRelease_date(String release_date) {
+        this.release_date = release_date;
+    }
+
+    public int getRevenue() {
+        return revenue;
+    }
+
+    public void setRevenue(int revenue) {
+        this.revenue = revenue;
+    }
+
+    public int getRuntime() {
+        return runtime;
+    }
+
+    public void setRuntime(int runtime) {
+        this.runtime = runtime;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getTagline() {
+        return tagline;
+    }
+
+    public void setTagline(String tagline) {
+        this.tagline = tagline;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public boolean isVideo() {
+        return video;
+    }
+
+    public void setVideo(boolean video) {
+        this.video = video;
+    }
+
+    public double getVote_average() {
+        return vote_average;
+    }
+
+    public void setVote_average(double vote_average) {
+        this.vote_average = vote_average;
+    }
+
+    public int getVote_count() {
+        return vote_count;
+    }
+
+    public void setVote_count(int vote_count) {
+        this.vote_count = vote_count;
+    }
+
+    public List<GenresBean> getGenres() {
+        return genres;
+    }
+
+    public void setGenres(List<GenresBean> genres) {
+        this.genres = genres;
+    }
+
+    public List<ProductionCompaniesBean> getProduction_companies() {
+        return production_companies;
+    }
+
+    public void setProduction_companies(List<ProductionCompaniesBean> production_companies) {
+        this.production_companies = production_companies;
+    }
+
+    public List<ProductionCountriesBean> getProduction_countries() {
+        return production_countries;
+    }
+
+    public void setProduction_countries(List<ProductionCountriesBean> production_countries) {
+        this.production_countries = production_countries;
+    }
+
+    public List<SpokenLanguagesBean> getSpoken_languages() {
+        return spoken_languages;
+    }
+
+    public void setSpoken_languages(List<SpokenLanguagesBean> spoken_languages) {
+        this.spoken_languages = spoken_languages;
+    }
+
+    public static class GenresBean {
+        /**
+         * id : 18
+         * name : Drama
+         */
+
+        private int id;
+        private String name;
+
+        public int getId() {
+            return id;
+        }
+
+        public void setId(int id) {
+            this.id = id;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+    }
+
+    public static class ProductionCompaniesBean {
+        /**
+         * name : Regency Enterprises
+         * id : 508
+         */
+
+        private String name;
+        private int id;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public int getId() {
+            return id;
+        }
+
+        public void setId(int id) {
+            this.id = id;
+        }
+    }
+
+    public static class ProductionCountriesBean {
+        /**
+         * iso_3166_1 : DE
+         * name : Germany
+         */
+
+        private String iso_3166_1;
+        private String name;
+
+        public String getIso_3166_1() {
+            return iso_3166_1;
+        }
+
+        public void setIso_3166_1(String iso_3166_1) {
+            this.iso_3166_1 = iso_3166_1;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+    }
+
+    public static class SpokenLanguagesBean {
+        /**
+         * iso_639_1 : en
+         * name : English
+         */
+
+        private String iso_639_1;
+        private String name;
+
+        public String getIso_639_1() {
+            return iso_639_1;
+        }
+
+        public void setIso_639_1(String iso_639_1) {
+            this.iso_639_1 = iso_639_1;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+    }
+}
