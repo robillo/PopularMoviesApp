b3bab4e3d2b24de1f77f4c53da66f801ff465dab
Robin Kamboj <dev.robinkamboj@gmail.com>
2017-03-09 20:20:10 +0530

    Initial commit

---
+++ app/src/main/java/com/appbusters/robinkamboj/popularmoviesapp/view/fragments/AllMoviesFragment.java
@@ -0,0 +1,31 @@
+package com.appbusters.robinkamboj.popularmoviesapp.view.fragments;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.appbusters.robinkamboj.popularmoviesapp.R;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class AllMoviesFragment extends Fragment {
+
+
+    public AllMoviesFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_all_movies, container, false);
+    }
+
+}
