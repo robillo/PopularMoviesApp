8c5234725a509d8a2f04129197e4c6786be82704
Robin Kamboj <dev.robinkamboj@gmail.com>
2017-03-10 01:10:24 +0530

    ...

---
+++ app/src/main/java/com/appbusters/robinkamboj/popularmoviesapp/rest/ApiInterface.java
@@ -0,0 +1,18 @@
+package com.appbusters.robinkamboj.popularmoviesapp.rest;
+
+import com.appbusters.robinkamboj.popularmoviesapp.model.MoviesResponse;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+import retrofit2.http.Query;
+
+public interface ApiInterface {
+
+    @GET("movie/top_rated")
+    Call<MoviesResponse> getTopRatedMovies(@Query("api_key") String apiKey);
+
+    @GET("movie/{id}")
+    Call<MoviesResponse> getMovieDetails(@Path("id") int id, @Query("api_key") String apiKey);
+
+}
