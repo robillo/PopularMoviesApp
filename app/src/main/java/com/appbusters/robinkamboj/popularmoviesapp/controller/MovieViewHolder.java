aa54a1778d9b1ba0a9efa76f3ead920f55497451
Robin Kamboj <dev.robinkamboj@gmail.com>
2017-03-18 15:20:06 +0530

    AllMoviesFragment

--- app/src/main/java/com/appbusters/robinkamboj/popularmoviesapp/controller/MovieViewHolder.java
+++ app/src/main/java/com/appbusters/robinkamboj/popularmoviesapp/controller/MovieViewHolder.java
@@ -2,6 +2,7 @@ package com.appbusters.robinkamboj.popularmoviesapp.controller;
 
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
@@ -11,10 +12,13 @@ public class MovieViewHolder extends RecyclerView.ViewHolder{
 
     LinearLayout moviesLayout;
     TextView movieTitle;
+    ImageView poster, alternate;
 
     public MovieViewHolder(View itemView) {
         super(itemView);
         moviesLayout = (LinearLayout) itemView.findViewById(R.id.movies_layout);
         movieTitle = (TextView) itemView.findViewById(R.id.title);
+        poster = (ImageView) itemView.findViewById(R.id.poster);
+        alternate = (ImageView) itemView.findViewById(R.id.alternate);
     }
 }
