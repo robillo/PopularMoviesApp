9aec0170755311a4d899fdfaeb10660a383814d7
Robin Kamboj <dev.robinkamboj@gmail.com>
2017-03-18 15:13:32 +0530

    AllMoviesFragment

--- app/src/main/java/com/appbusters/robinkamboj/popularmoviesapp/view/fragments/AllMoviesFragment.java
+++ app/src/main/java/com/appbusters/robinkamboj/popularmoviesapp/view/fragments/AllMoviesFragment.java
@@ -1,13 +1,16 @@
 package com.appbusters.robinkamboj.popularmoviesapp.view.fragments;
 
 
+import android.media.ImageReader;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import com.appbusters.robinkamboj.popularmoviesapp.R;
 import com.appbusters.robinkamboj.popularmoviesapp.controller.MoviesAdapter;
@@ -34,6 +37,7 @@ public class AllMoviesFragment extends Fragment {
     private static final String API_KEY = "13befb0c6409e8c61c5e9ec4265a1d1c";
     private RecyclerView recyclerView;
     private MoviesAdapter adapter;
+    private ImageView alternate_layout;
 
     public AllMoviesFragment() {
         // Required empty public constructor
@@ -45,7 +49,10 @@ public class AllMoviesFragment extends Fragment {
         // Inflate the layout for this fragment
         View v = inflater.inflate(R.layout.fragment_all_movies, container, false);
 
+        alternate_layout = (ImageView) v.findViewById(R.id.alternate_layout);
         recyclerView = (RecyclerView) v.findViewById(R.id.recyclerview);
+        GridLayoutManager gridLayoutManager = new GridLayoutManager(getActivity().getApplicationContext(), 2);
+        recyclerView.setLayoutManager(gridLayoutManager);
 
         ApiInterface apiService = ApiClient.getClient().create(ApiInterface.class);
         Call<MoviesResponse> call = apiService.getTopRatedMovies(API_KEY);
@@ -55,6 +62,10 @@ public class AllMoviesFragment extends Fragment {
                 int statusCode = response.code();
                 List<Movie> movies = response.body().getResults();
                 adapter = new MoviesAdapter(movies, R.layout.row_layout, getActivity().getApplicationContext());
+                if(adapter.getItemCount()>0){
+                    recyclerView.setVisibility(View.VISIBLE);
+                    alternate_layout.setVisibility(View.INVISIBLE);
+                }
                 recyclerView.setAdapter(adapter);
             }
 
