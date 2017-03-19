3d422c554af07232c30e9eb44d918ed28794af76
Robin Kamboj <dev.robinkamboj@gmail.com>
2017-03-19 13:15:30 +0530

    AllMoviesFragment

--- app/src/main/java/com/appbusters/robinkamboj/popularmoviesapp/controller/MovieViewHolder.java
+++ app/src/main/java/com/appbusters/robinkamboj/popularmoviesapp/controller/MovieViewHolder.java
@@ -4,6 +4,7 @@ import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.appbusters.robinkamboj.popularmoviesapp.R;
