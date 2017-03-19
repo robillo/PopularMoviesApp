6ffb717c3cf2ff69465f49816c6be6635775f6ec
Robin Kamboj <dev.robinkamboj@gmail.com>
2017-03-19 13:32:27 +0530

    Detail from Adapter

---
+++ app/src/main/java/com/appbusters/robinkamboj/popularmoviesapp/view/activities/DetailActivity.java
@@ -0,0 +1,15 @@
+package com.appbusters.robinkamboj.popularmoviesapp.view.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.appbusters.robinkamboj.popularmoviesapp.R;
+
+public class DetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+    }
+}
