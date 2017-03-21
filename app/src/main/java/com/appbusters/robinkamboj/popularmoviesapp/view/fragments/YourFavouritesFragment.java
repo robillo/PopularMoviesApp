0c56f69dc4fdcfc80b612e0d8cf46799a36276a9
Robin Kamboj <dev.robinkamboj@gmail.com>
2017-03-21 21:06:22 +0530

    ...

--- app/src/main/java/com/appbusters/robinkamboj/popularmoviesapp/view/fragments/YourFavouritesFragment.java
+++ app/src/main/java/com/appbusters/robinkamboj/popularmoviesapp/view/fragments/YourFavouritesFragment.java
@@ -4,6 +4,9 @@ package com.appbusters.robinkamboj.popularmoviesapp.view.fragments;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
@@ -25,7 +28,32 @@ public class YourFavouritesFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_your_favourites, container, false);
+        View v = inflater.inflate(R.layout.fragment_your_favourites, container, false);
+
+        setHasOptionsMenu(true);
+
+        return v;
     }
 
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        inflater.inflate(R.menu.menu_detail, menu);
+        super.onCreateOptionsMenu(menu,inflater);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
 }
