ad263cbc6e955e6263369210a41366e88000219f
Robin Kamboj <dev.robinkamboj@gmail.com>
2017-03-21 20:46:12 +0530

    disabled activity toolbar

--- app/src/main/java/com/appbusters/robinkamboj/popularmoviesapp/controller/MoviesAdapter.java
+++ app/src/main/java/com/appbusters/robinkamboj/popularmoviesapp/controller/MoviesAdapter.java
@@ -39,7 +39,7 @@ public class MoviesAdapter extends RecyclerView.Adapter<MovieViewHolder>{
         Glide.with(context).load("http://image.tmdb.org/t/p/w185" + path)
                 .diskCacheStrategy(DiskCacheStrategy.ALL)
                 .crossFade()
-                .fitCenter()
+                .centerCrop()
                 .placeholder(R.drawable.placeholder)
                 .into(holder.poster);
         Log.e("POSTER PATH: ", path);
