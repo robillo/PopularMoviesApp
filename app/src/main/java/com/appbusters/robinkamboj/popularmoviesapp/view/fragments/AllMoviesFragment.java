b9da194b85d773bc5c305c9cbb3f0e14e1226531
Robin Kamboj <dev.robinkamboj@gmail.com>
2017-03-22 16:37:21 +0530

    ...

--- app/src/main/java/com/appbusters/robinkamboj/popularmoviesapp/view/fragments/AllMoviesFragment.java
+++ app/src/main/java/com/appbusters/robinkamboj/popularmoviesapp/view/fragments/AllMoviesFragment.java
@@ -131,10 +131,50 @@ public class AllMoviesFragment extends Fragment {
                                     }
                                     case 1:{
                                         Log.e("which?", "Most Popular");
+                                        apiService = ApiClient.getClient().create(ApiInterface.class);
+                                        Call<MoviesResponse> call = apiService.getMostPopularMovies(API_KEY);
+                                        call.enqueue(new Callback<MoviesResponse>() {
+                                            @Override
+                                            public void onResponse(Call<MoviesResponse> call, Response<MoviesResponse> response) {
+                                                if(response.body()!=null){
+                                                    List<Movie> movies = response.body().getResults();
+                                                    adapter = new MoviesAdapter(movies, R.layout.row_layout, getActivity().getApplicationContext());
+                                                    if(adapter.getItemCount()>0){
+                                                        recyclerView.setAdapter(adapter);
+                                                        alternate_layout.setVisibility(View.INVISIBLE);
+                                                    }
+                                                }
+                                            }
+
+                                            @Override
+                                            public void onFailure(Call<MoviesResponse> call, Throwable t) {
+
+                                            }
+                                        });
                                         break;
                                     }
                                     case 2:{
                                         Log.e("which?", "Most Rated");
+                                        apiService = ApiClient.getClient().create(ApiInterface.class);
+                                        Call<MoviesResponse> call = apiService.getMostRatedMovies(API_KEY);
+                                        call.enqueue(new Callback<MoviesResponse>() {
+                                            @Override
+                                            public void onResponse(Call<MoviesResponse> call, Response<MoviesResponse> response) {
+                                                if(response.body()!=null){
+                                                    List<Movie> movies = response.body().getResults();
+                                                    adapter = new MoviesAdapter(movies, R.layout.row_layout, getActivity().getApplicationContext());
+                                                    if(adapter.getItemCount()>0){
+                                                        recyclerView.setAdapter(adapter);
+                                                        alternate_layout.setVisibility(View.INVISIBLE);
+                                                    }
+                                                }
+                                            }
+
+                                            @Override
+                                            public void onFailure(Call<MoviesResponse> call, Throwable t) {
+
+                                            }
+                                        });
                                         break;
                                     }
                                 }
