05e16ff4a030e1674dfc83c4e7d951087954affc
Robin Kamboj <dev.robinkamboj@gmail.com>
2017-04-01 20:04:56 +0530

    videos VH

---
+++ app/src/main/java/com/appbusters/robinkamboj/popularmoviesapp/controller/VideosHolder.java
@@ -0,0 +1,20 @@
+package com.appbusters.robinkamboj.popularmoviesapp.controller;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+
+import com.appbusters.robinkamboj.popularmoviesapp.R;
+
+public class VideosHolder extends RecyclerView.ViewHolder{
+
+    private ImageView backDrop;
+    private RelativeLayout clickView;
+
+    public VideosHolder(View itemView) {
+        super(itemView);
+        backDrop = (ImageView) itemView.findViewById(R.id.back_drop);
+        clickView = (RelativeLayout) itemView.findViewById(R.id.click_view);
+    }
+}
