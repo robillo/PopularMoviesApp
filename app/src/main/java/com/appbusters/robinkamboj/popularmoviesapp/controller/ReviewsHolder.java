df1358f40be48c8fd0b93a18c1a29b41ded8aed3
Robin Kamboj <dev.robinkamboj@gmail.com>
2017-04-02 16:05:45 +0530

    ItemClickListener

--- app/src/main/java/com/appbusters/robinkamboj/popularmoviesapp/controller/ReviewsHolder.java
+++ app/src/main/java/com/appbusters/robinkamboj/popularmoviesapp/controller/ReviewsHolder.java
@@ -6,13 +6,32 @@ import android.widget.TextView;
 
 import com.appbusters.robinkamboj.popularmoviesapp.R;
 
-public class ReviewsHolder extends RecyclerView.ViewHolder{
+public class ReviewsHolder extends RecyclerView.ViewHolder implements View.OnClickListener, View.OnLongClickListener{
 
     public TextView review_description, review_reviewee;
+    private ItemClickListener clickListener;
 
     public ReviewsHolder(View itemView) {
         super(itemView);
         review_description = (TextView) itemView.findViewById(R.id.description);
         review_reviewee = (TextView) itemView.findViewById(R.id.reviewee);
+
+        itemView.setOnClickListener(this);
+        itemView.setOnLongClickListener(this);
+    }
+
+    public void setClickListener(ItemClickListener clickListener){
+        this.clickListener = clickListener;
+    }
+
+    @Override
+    public void onClick(View view) {
+        clickListener.onClick(view, getAdapterPosition(), false);
+    }
+
+    @Override
+    public boolean onLongClick(View view) {
+        clickListener.onClick(view, getAdapterPosition(), true);
+        return false;
     }
 }
