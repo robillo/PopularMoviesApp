ded61872dffa2a73cb65d911a822635ef549b807
Robin Kamboj <dev.robinkamboj@gmail.com>
2017-04-02 16:00:19 +0530

    ItemClickListener

--- app/src/main/java/com/appbusters/robinkamboj/popularmoviesapp/controller/VideosHolder.java
+++ app/src/main/java/com/appbusters/robinkamboj/popularmoviesapp/controller/VideosHolder.java
@@ -7,14 +7,33 @@ import android.widget.RelativeLayout;
 
 import com.appbusters.robinkamboj.popularmoviesapp.R;
 
-public class VideosHolder extends RecyclerView.ViewHolder{
+public class VideosHolder extends RecyclerView.ViewHolder implements View.OnClickListener, View.OnLongClickListener{
 
     public ImageView backDrop;
     public RelativeLayout clickView;
+    private ItemClickListener clickListener;
 
     public VideosHolder(View itemView) {
         super(itemView);
         backDrop = (ImageView) itemView.findViewById(R.id.back_drop);
         clickView = (RelativeLayout) itemView.findViewById(R.id.click_view);
+
+        itemView.setOnClickListener(this);
+        itemView.setOnLongClickListener(this);
+    }
+
+    public void setClickListener(ItemClickListener clickListener){
+        this.clickListener = clickListener;
+    }
+
+    @Override
+    public void onClick(View view) {
+        clickListener.onClick(view, getAdapterPosition(), false);
+    }
+
+    @Override
+    public boolean onLongClick(View view) {
+        clickListener.onClick(view, getAdapterPosition(), true);
+        return false;
     }
 }
