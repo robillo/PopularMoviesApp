62e2653cbf11dd6e544aa60a17c2ebf026df8660
Robin Kamboj <dev.robinkamboj@gmail.com>
2017-04-03 20:40:52 +0530

    onCreate() and onUpgrade()

--- app/src/main/java/com/appbusters/robinkamboj/popularmoviesapp/controller/MyDBHelper.java
+++ app/src/main/java/com/appbusters/robinkamboj/popularmoviesapp/controller/MyDBHelper.java
@@ -16,6 +16,14 @@ public class MyDBHelper extends SQLiteOpenHelper {
 
     private static final int DATABASE_VERSION = 1;
     private static final String DATABASE_NAME = "Movies.db";
+    private static final String TABLE_NAME = "FAVORITE_MOVIES";
+    private static final String ID = "ID";
+    private static final String NAME = "NAME";
+    private static final String POSTER = "POSTER";
+    private static final String BACKDROP = "BACKDROP";
+    private static final String RATING = "RATING";
+    private static final String RELEASE_DATE = "DATE";
+    private static final String OVERVIEW = "OVERVIEW";
 
     public MyDBHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
@@ -23,11 +31,12 @@ public class MyDBHelper extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase sqLiteDatabase) {
-
+        sqLiteDatabase.execSQL("CREATE TABLE " + TABLE_NAME + " (" + ID + " INTEGER PRIMARY KEY, " + NAME + " TEXT, " + POSTER + " TEXT, " + BACKDROP + " TEXT, " + RATING + " TEXT, " + RELEASE_DATE + " TEXT, " + OVERVIEW + " OVERVIEW)");
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase sqLiteDatabase, int oldVersion, int newVersion) {
-
+        sqLiteDatabase.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
+        onCreate(sqLiteDatabase);
     }
 }
