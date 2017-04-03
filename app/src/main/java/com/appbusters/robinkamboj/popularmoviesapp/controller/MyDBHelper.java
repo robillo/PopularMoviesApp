8e2a60a85d002f9e92e91e3ac62cefed72a93e9d
Robin Kamboj <dev.robinkamboj@gmail.com>
2017-04-03 22:12:47 +0530

    MyDBHelper

--- app/src/main/java/com/appbusters/robinkamboj/popularmoviesapp/controller/MyDBHelper.java
+++ app/src/main/java/com/appbusters/robinkamboj/popularmoviesapp/controller/MyDBHelper.java
@@ -6,11 +6,15 @@ import android.database.Cursor;
 import android.database.DatabaseUtils;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.support.annotation.NonNull;
 
 import com.appbusters.robinkamboj.popularmoviesapp.model.Movie;
 
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
 import java.util.List;
+import java.util.ListIterator;
 
 public class MyDBHelper extends SQLiteOpenHelper {
 
@@ -71,4 +75,147 @@ public class MyDBHelper extends SQLiteOpenHelper {
         SQLiteDatabase sqLiteDatabase = this.getWritableDatabase();
         return sqLiteDatabase.delete(TABLE_NAME, ID + " =? ", new String[]{Integer.toString(id)});
     }
+
+    public List<Movie> getAllMovies() {
+        List<Movie> data = new ArrayList<>();
+
+        SQLiteDatabase sqLiteDatabase = this.getReadableDatabase();
+        Cursor res =  sqLiteDatabase.rawQuery( "SELECT * FROM " + TABLE_NAME + " ", null );
+        res.moveToFirst();
+
+        while(res.isAfterLast() == false){
+            List<Integer> i = new List<Integer>() {
+                @Override
+                public int size() {
+                    return 0;
+                }
+
+                @Override
+                public boolean isEmpty() {
+                    return false;
+                }
+
+                @Override
+                public boolean contains(Object o) {
+                    return false;
+                }
+
+                @NonNull
+                @Override
+                public Iterator<Integer> iterator() {
+                    return null;
+                }
+
+                @NonNull
+                @Override
+                public Object[] toArray() {
+                    return new Object[0];
+                }
+
+                @NonNull
+                @Override
+                public <T> T[] toArray(@NonNull T[] ts) {
+                    return null;
+                }
+
+                @Override
+                public boolean add(Integer integer) {
+                    return false;
+                }
+
+                @Override
+                public boolean remove(Object o) {
+                    return false;
+                }
+
+                @Override
+                public boolean containsAll(@NonNull Collection<?> collection) {
+                    return false;
+                }
+
+                @Override
+                public boolean addAll(@NonNull Collection<? extends Integer> collection) {
+                    return false;
+                }
+
+                @Override
+                public boolean addAll(int i, @NonNull Collection<? extends Integer> collection) {
+                    return false;
+                }
+
+                @Override
+                public boolean removeAll(@NonNull Collection<?> collection) {
+                    return false;
+                }
+
+                @Override
+                public boolean retainAll(@NonNull Collection<?> collection) {
+                    return false;
+                }
+
+                @Override
+                public void clear() {
+
+                }
+
+                @Override
+                public Integer get(int i) {
+                    return null;
+                }
+
+                @Override
+                public Integer set(int i, Integer integer) {
+                    return null;
+                }
+
+                @Override
+                public void add(int i, Integer integer) {
+
+                }
+
+                @Override
+                public Integer remove(int i) {
+                    return null;
+                }
+
+                @Override
+                public int indexOf(Object o) {
+                    return 0;
+                }
+
+                @Override
+                public int lastIndexOf(Object o) {
+                    return 0;
+                }
+
+                @Override
+                public ListIterator<Integer> listIterator() {
+                    return null;
+                }
+
+                @NonNull
+                @Override
+                public ListIterator<Integer> listIterator(int i) {
+                    return null;
+                }
+
+                @NonNull
+                @Override
+                public List<Integer> subList(int i, int i1) {
+                    return null;
+                }
+            };
+            data.add(new Movie(res.getString(res.getColumnIndex(POSTER)), false, res.getString(res.getColumnIndex(OVERVIEW)), res.getString(res.getColumnIndex(RELEASE_DATE)), res.getInt(res.getColumnIndex(ID)), res.getString(res.getColumnIndex(NAME)), "null", res.getString(res.getColumnIndex(NAME)), res.getString(res.getColumnIndex(BACKDROP)), 0, 0, false, 0, i));
+            res.moveToNext();
+        }
+
+        return data;
+    }
+
+    public int rowsCount(){
+        SQLiteDatabase sqLiteDatabase = this.getReadableDatabase();
+        int numRows = (int) DatabaseUtils.queryNumEntries(sqLiteDatabase, TABLE_NAME);
+        return numRows;
+    };
+
 }
